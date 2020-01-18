798d6068bce956d5239d841533dd60a8845f1eec
Robert E Fry <robert.e.fry@protonmail.com>
2020-01-18 01:23:32 +0000

    Refactor to use OpenGL vertex buffers

--- src/main.cpp
+++ src/main.cpp
@@ -37,22 +37,28 @@ int main(void)
     /* Print the welcome message */
     std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
 
+    /* Generate a new vertex buffer */
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+
+    /* Put vertices into the vertex buffer */
+    float vertices[] = { -0.5f, -0.5f, +0.0f, +0.5f, +0.5f, -0.5f };
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Specify the vertex attributes */
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        {
-            glColor3f(1.0f, 0.0f, 0.0f);
-            glVertex2f(-0.5f, -0.5f);
-            glColor3f(0.0f, 1.0f, 0.0f);
-            glVertex2f( 0.0f,  0.5f);
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glVertex2f( 0.5f, -0.5f);
-        }
-        glEnd();
+        glBindBuffer(GL_ARRAY_BUFFER, buffer);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
